0f729030b98439fe04b02cac96b1affafda06f2b
Neal Anderson <nandersons@Neals-MacBook-Pro.local>
2016-06-12 23:11:02 -0500

    commiting completed resume

--- js/helper.js
+++ js/helper.js
@@ -26,34 +26,36 @@ var HTMLlocation = '<li class="flex-item"><span class="orange-text">location</sp
 var HTMLbioPic = '<img src="%data%" class="biopic">';
 var HTMLwelcomeMsg = '<span class="welcome-message">%data%</span>';
 
-var HTMLskillsStart = '<h3 id="skills-h3">Skills at a Glance:</h3><ul id="skills" class="flex-box"></ul>';
+var HTMLskillsStart = '<h3 id="skills-h3" style="margin-top: 50px">Skills at a Glance:</h3><ul id="skills" class="flex-box"></ul>';
 var HTMLskills = '<li class="flex-item"><span class="white-text">%data%</span></li>';
 
 var HTMLworkStart = '<div class="work-entry"></div>';
-var HTMLworkEmployer = '<a href="#">%data%';
+var HTMLworkEmployer = '<a href="http://sequoiacg.com/" target="_blank">%data%';
 var HTMLworkTitle = ' - %data%</a>';
 var HTMLworkDates = '<div class="date-text">%data%</div>';
 var HTMLworkLocation = '<div class="location-text">%data%</div>';
 var HTMLworkDescription = '<p><br>%data%</p>';
 
 var HTMLprojectStart = '<div class="project-entry"></div>';
-var HTMLprojectTitle = '<a href="#">%data%</a>';
+var HTMLprojectTitle = '<a href="#" style="margin-top: 0px; margin-bottom: 10px;">%data%</a>';
 var HTMLprojectDates = '<div class="date-text">%data%</div>';
 var HTMLprojectDescription = '<p><br>%data%</p>';
 var HTMLprojectImage = '<img src="%data%">';
 
 var HTMLschoolStart = '<div class="education-entry"></div>';
-var HTMLschoolName = '<a href="#">%data%';
+var HTMLschoolName = '<a href="http://siu.edu/" target="_blank">%data%';
 var HTMLschoolDegree = ' -- %data%</a>';
 var HTMLschoolDates = '<div class="date-text">%data%</div>';
 var HTMLschoolLocation = '<div class="location-text">%data%</div>';
 var HTMLschoolMajor = '<em><br>Major: %data%</em>';
 
 var HTMLonlineClasses = '<h3>Online Classes</h3>';
-var HTMLonlineTitle = '<a href="#">%data%';
+var HTMLonlineTitle = '%data%';
 var HTMLonlineSchool = ' - %data%</a>';
 var HTMLonlineDates = '<div class="date-text">%data%</div>';
-var HTMLonlineURL = '<br><a href="#">%data%</a>';
+var HTMLonlineURL1 = '<br><a href="https://classroom.udacity.com/courses/cs101" target="_blank" style="margin-top: 0px; margin-bottom: 10px">%data%</a>';
+var HTMLonlineURL2 = '<br><a href="https://classroom.udacity.com/courses/ud804" target="_blank" style="margin-top: 0px; margin-bottom: 10px">%data%</a>';
+var HTMLonlineURL3 = '<br><a href="https://classroom.udacity.com/courses/ud304" target="_blank" style="margin-top: 0px; margin-bottom: 10px">%data%</a>';
 
 var internationalizeButton = '<button>Internationalize</button>';
 var googleMap = '<div id="map"></div>';
@@ -110,9 +112,9 @@ function initializeMap() {
     disableDefaultUI: true
   };
 
-  /* 
+  /*
   For the map to be displayed, the googleMap var must be
-  appended to #mapDiv in resumeBuilder.js. 
+  appended to #mapDiv in resumeBuilder.js.
   */
   map = new google.maps.Map(document.querySelector('#map'), mapOptions);
 
@@ -131,7 +133,7 @@ function initializeMap() {
 
     // iterates through school locations and appends each location to
     // the locations array. Note that forEach is used for array iteration
-    // as described in the Udacity FEND Style Guide: 
+    // as described in the Udacity FEND Style Guide:
     // https://udacity.github.io/frontend-nanodegree-styleguide/javascript.html#for-in-loop
     education.schools.forEach(function(school){
       locations.push(school.location);
@@ -139,7 +141,7 @@ function initializeMap() {
 
     // iterates through work locations and appends each location to
     // the locations array. Note that forEach is used for array iteration
-    // as described in the Udacity FEND Style Guide: 
+    // as described in the Udacity FEND Style Guide:
     // https://udacity.github.io/frontend-nanodegree-styleguide/javascript.html#for-in-loop
     work.jobs.forEach(function(job){
       locations.push(job.location);
@@ -177,7 +179,7 @@ function initializeMap() {
 
     // hmmmm, I wonder what this is about...
     google.maps.event.addListener(marker, 'click', function() {
-      // your code goes here!
+      infoWindow.open(map, marker);
     });
 
     // this is where the pin actually gets added to the map.
@@ -239,11 +241,11 @@ Uncomment the code below when you're ready to implement a Google Map!
 */
 
 // Calls the initializeMap() function when the page loads
-//window.addEventListener('load', initializeMap);
+window.addEventListener('load', initializeMap);
 
 // Vanilla JS way to listen for resizing of the window
 // and adjust map bounds
-//window.addEventListener('resize', function(e) {
+window.addEventListener('resize', function(e) {
   //Make sure the map bounds get updated on page resize
-//  map.fitBounds(mapBounds);
-//});
+  map.fitBounds(mapBounds);
+});
--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,197 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+//header
+var name = "Neal Anderson";
+var role = "Web Developer";
+
+var formattedName = HTMLheaderName.replace("%data%", name);
+
+var formattedRole = HTMLheaderRole.replace("%data%", role);
+
+$("#header").prepend(formattedName);
+$("#header").append(formattedRole);
+
+//contact info JSON
+var bio = {
+    "name" : "Neal Anderson",
+    "role" : "Front End Web Developer",
+    "contacts" : {
+        "email" : "NealAndersons@gmail.com",
+        "github" : "https://github.com/nandersons",
+        "mobile" : "217-652-0800",
+        "location" : "Springfield, Illinois"
+    },
+    "picture" : "images/mypic.JPG",
+    "welcome_message" : "Welcome to my Resume",
+    "skills" : ["HTML", " CSS", " JavaScript"],
+    display : function myFunction() {}
+};
+
+//contact info
+if (bio.skills.length > 0) {
+    var formattedBioPic = HTMLbioPic.replace("%data%", bio.picture);
+    $("#header").append(formattedBioPic);
+    var formattedWelcomeMsg = HTMLwelcomeMsg.replace("%data%", bio.welcome_message);
+    $("#header").append(formattedWelcomeMsg);
+    var formattedEmail = HTMLemail.replace("%data%", bio.contacts.email);
+    $("#topContacts").append(formattedEmail);
+    var formattedGithub = HTMLgithub.replace("%data%", bio.contacts.github);
+    $("#topContacts").append(formattedGithub);
+    var formattedMobile = HTMLmobile.replace("%data%", bio.contacts.mobile);
+    $("#topContacts").append(formattedMobile);
+    var formattedLocation = HTMLlocation.replace("%data%", bio.contacts.location);
+    $("#topContacts").append(formattedLocation);
+    //var formattedHeader = HTMLheaderName.replace("%data%", bio.hi);
+    //$("#header").append(formattedHeader);
+    $("#header").append(HTMLskillsStart);
+    var formattedSkill = HTMLskills.replace("%data%", bio.skills[0]);
+    $("#skills").append(formattedSkill);
+    formattedSkill = HTMLskills.replace("%data%", bio.skills[1]);
+    $("#skills").append(formattedSkill);
+    formattedSkill = HTMLskills.replace("%data%", bio.skills[2]);
+    $("#skills").append(formattedSkill);
+};
+
+//work JSON
+var work = {
+    "current_job" : "Consultant",
+    "employer" : "Sequoia Consulting Group",
+    "years_worked" : "January 2015 - Present",
+    "location" : "Springfield, Illinois",
+    "descript" : "In the midst of one of the worst financial decades in Illinois, our expert consulting services secures millions of dollars in federal funding every year for our client and helps keep the doors open at their agency.",
+    display : function myFunction() {}
+};
+
+//work
+if (work.current_job.length > 0) {
+    $("#workExperience").append(HTMLworkStart);
+    var formattedEmployer = HTMLworkEmployer.replace("%data%", work.employer);
+    var formattedTitle = HTMLworkTitle.replace("%data%", work.current_job);
+    var formattedEmployerTitle = formattedEmployer + formattedTitle;
+    $(".work-entry:last").append(formattedEmployerTitle);
+    var formattedDates = HTMLworkDates.replace("%data%", work.years_worked);
+    $(".work-entry:last").append(formattedDates);
+    var formattedLocation = HTMLworkLocation.replace("%data%", work.location);
+    $(".work-entry:last").append(formattedLocation);
+    var formattedDescription = HTMLworkDescription.replace("%data%", work.descript);
+    $(".work-entry:last").append(formattedDescription);
+};
+
+//projects JSON
+var projects = {
+    "portfolio" : "my portfolio",
+    display : function myFunction() {}
+};
+
+
+//projects
+if (projects.portfolio.length > 0) {
+    $("#projects").append(HTMLprojectStart);
+    var formattedTitle = HTMLprojectTitle.replace("%data%", projects.portfolio);
+    $(".project-entry:last").append(formattedTitle);
+};
+//var education = {};
+//education["name"] = "Southern Illinois University Carbondale";
+//education["years"] = "2008-2015";
+
+//$("#main").append(work["current_job"]);
+
+//education JSON
+var education = {
+    "schools" : {
+        "school": "Southern Illinois University Carbondale",
+        "location" : "Carbondale, Illinois",
+        "dates" : "2008-2015",
+        "majors" : {
+            "undergraduate" : "Criminology/Criminal Justice, BA",
+            "graduate" : "Masters of Public Administration, MPA",
+            "minor" : "Forensic Science, Minor"
+        },
+    },
+    "onlineCourses" :
+    [{
+        "course" : "Intro to Computer Science",
+        "school" : "Udacity",
+        "url" : "https://classroom.udacity.com/courses/cs101"
+    },
+    {
+        "course" : "JavaScript Basics",
+        "school" : "Udacity",
+        "url" : "https://classroom.udacity.com/courses/ud804"
+    },
+    {
+        "course" : "Intro to HTML and CSS",
+        "school" : "Udacity",
+        "url" : "https://classroom.udacity.com/courses/ud304"
+    }],
+    display : function myFunction() {}
+};
+
+//education
+if (education.schools.school.length > 0) {
+    $("#education").append(HTMLschoolStart);
+    var formattedSchool = HTMLschoolName.replace("%data%", education.schools.school);
+    $(".education-entry:last").append(formattedSchool);
+    var formattedSchoolDegreeU = HTMLschoolDegree.replace("%data%", education.schools.majors.undergraduate);
+    $(".education-entry:last").append(formattedSchoolDegreeU);
+    var formattedSchoolDegreeM = HTMLschoolDegree.replace("%data%", education.schools.majors.graduate);
+    $(".education-entry:last").append(formattedSchoolDegreeM);
+    var formattedSchoolDegreeP = HTMLschoolDegree.replace("%data%", education.schools.majors.minor);
+    $(".education-entry:last").append(formattedSchoolDegreeP);
+    var formattedSchoolDates = HTMLschoolDates.replace("%data%", education.schools.dates);
+    $(".education-entry:last").append(formattedSchoolDates);
+    var formattedSchoolLocation = HTMLschoolLocation.replace("%data%", education.schools.location);
+    $(".education-entry:last").append(formattedSchoolLocation);
+    $(".education-entry:last").append(HTMLonlineClasses);
+    var formattedTitle = HTMLonlineTitle.replace("%data%", education.onlineCourses[0].course);
+    $(".education-entry:last").append(formattedTitle);
+    var formattedOnlineSchool = HTMLonlineSchool.replace("%data%", education.onlineCourses[0].school);
+    $(".education-entry:last").append(formattedOnlineSchool);
+    var formattedURL1 = HTMLonlineURL1.replace("%data%", education.onlineCourses[0].url);
+    $(".education-entry:last").append(formattedURL1);
+
+    var formattedTitle = HTMLonlineTitle.replace("%data%", education.onlineCourses[1].course);
+    $(".education-entry:last").append(formattedTitle);
+    var formattedOnlineSchool = HTMLonlineSchool.replace("%data%", education.onlineCourses[1].school);
+    $(".education-entry:last").append(formattedOnlineSchool);
+    var formattedURL2 = HTMLonlineURL2.replace("%data%", education.onlineCourses[1].url);
+    $(".education-entry:last").append(formattedURL2);
+
+    var formattedTitle = HTMLonlineTitle.replace("%data%", education.onlineCourses[2].course);
+    $(".education-entry:last").append(formattedTitle);
+    var formattedOnlineSchool = HTMLonlineSchool.replace("%data%", education.onlineCourses[2].school);
+    $(".education-entry:last").append(formattedOnlineSchool);
+    var formattedURL3 = HTMLonlineURL3.replace("%data%", education.onlineCourses[2].url);
+    $(".education-entry:last").append(formattedURL3);
+};
+
+//footer
+if (bio.skills.length > 0) {
+    var formattedEmail = HTMLemail.replace("%data%", bio.contacts.email);
+    $("#footerContacts").append(formattedEmail);
+    var formattedGithub = HTMLgithub.replace("%data%", bio.contacts.github);
+    $("#footerContacts").append(formattedGithub);
+    var formattedMobile = HTMLmobile.replace("%data%", bio.contacts.mobile);
+    $("#footerContacts").append(formattedMobile);
+    var formattedLocation = HTMLlocation.replace("%data%", bio.contacts.location);
+    $("#footerContacts").append(formattedLocation);
+}
+//Logs X and Y locations of user clicks on the page
+$(document).click(function(loc) {
+    var x = loc.pageX;
+    var y = loc.pageY;
+    logClicks(x, y);
+});
+
+//Creats a clickable button that converts my name on the page to an "internationalized" version of itself
+$("#main").append(internationalizeButton);
+function inName(name) {
+    nameArray = name.trim().split(" ");
+    first = nameArray[0];
+    last = nameArray[1];
+    return (first.charAt(0).toUpperCase() + first.slice(1)) + " " + (last.toUpperCase());
+};
+
+function mapFunction() {
+    $("#mapDiv").append(googleMap);
+}
+
+mapFunction()
